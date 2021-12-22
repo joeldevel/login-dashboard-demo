ac948dc1ec9ebdd099efba9b6d80f27b6dbf6400
joeldevel <christianjoel.developer@gmail.com>
2021-12-22 18:32:17 -0300

    add fields to create_users_table migration

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,7 +15,11 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('username')->unique;
+            $table->string('firstname')->nullable();
+            $table->string('lastname')->nullable();
+            $table->string('dni')->nullable();
+            $table->string('userimg')->nullable();
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
