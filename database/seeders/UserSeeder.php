25e3667e73910cf12ac6f8d639825a91dbe983dd
joeldevel <christianjoel.developer@gmail.com>
2021-12-22 18:41:37 -0300

    adds UserSeeder with two users for testing

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+      DB::table('users')->insert([
+          'username' => 'user1',
+          'email' => 'user1@gmail.com',
+          'firstname' =>'user1firstname',
+          'lastname' =>'user1lastname',
+          'dni' => '123123',
+          'userimg' => "",
+          'password' => bcrypt(1234),
+      ]);
+
+      DB::table('users')->insert([
+          'username' => 'user2',
+          'email' => 'user2@gmail.com',
+          'firstname' =>'user2firstname',
+          'lastname' =>'user2lastname',
+          'dni' => '333666',
+          'userimg' => "",
+          'password' => bcrypt('abcd'),
+      ]);
+    }
+}
