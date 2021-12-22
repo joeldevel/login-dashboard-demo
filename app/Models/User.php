01a18c0b3e1eb3ec4be9621c4cb281d147320545
joeldevel <christianjoel.developer@gmail.com>
2021-12-22 18:38:51 -0300

    adds laravel sanctum for authentication. adds api routes

--- app/Models/User.php
+++ app/Models/User.php
@@ -18,7 +18,11 @@ class User extends Authenticatable
      * @var array<int, string>
      */
     protected $fillable = [
-        'name',
+        'username',
+        'firstname',
+        'lastname',
+        'dni',
+        'userimg',
         'email',
         'password',
     ];
--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,8 @@
 <?php
 
+use App\Models\User;
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -14,6 +17,13 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+// protected routes
+Route::group(['middleware'=>['auth:sanctum']], function(){
+    Route::put('/user/{id}', [UserController::class, 'update']);
+    Route::post('/user/{id}', [UserController::class, 'show']);
+    Route::post('/logout', [AuthController::class, 'logout']);
 });
+
+// public routes
+Route::post('/login', [AuthController::class, 'login']);
+Route::post('/register', [AuthController::class, 'register']);
